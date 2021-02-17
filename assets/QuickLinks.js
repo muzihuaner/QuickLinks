d01bfbe47109b225086c0758a972eb106dfd7956
木子欢儿～ <2209514944@qq.com>
2021-02-17 19:15:55 +0800

    Add files via upload

---
+++ assets/QuickLinks.js
@@ -0,0 +1,20 @@
+function travelling(){
+    var url=new Array();
+
+    url[0]="https://github.com/muzihuaner/QuickLinks";
+    url[1]="https://quickso.cn";
+    url[2]="https://huangenet.cn";
+    url[3]="https://zhiurl.cn";
+    url[4]="https://www.cnblogs.com/HGNET/";
+   
+
+
+    if (document.referrer) {
+      var origin = new URL(document.referrer).origin;
+      url.splice(url.indexOf(origin), 1);
+    }
+    
+    
+    var ints=Math.floor(Math.random() * url.length);
+    window.location=url[ints];
+   }
